2c2060326d3770975489f538ba9266780ea3aa08
lightyen <lightyen0123@gmail.com>
2020-10-26 17:19:16 +0800

    add svgr

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,9 +3,12 @@ import Page from "~/components/Page"
 import "twin.macro"
 import "dayjs/locale/zh-tw"
 
+import ReactLogo from "~/assets/logo.svg"
+
 const HomePage = () => {
 	return (
 		<Page>
+			<ReactLogo tw="w-32" />
 			<FormattedMessage id="test" values={{ name: "React" }} />
 		</Page>
 	)
